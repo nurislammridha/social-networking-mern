976f49a12e40b77ea5636ff7ee12705491be2211
nurislammridha <mridha851@gmail.com>
2024-01-18 15:08:40 +0600

    up

--- backend/Routes/api/message.js
+++ backend/Routes/api/message.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth")
 const allMessage = require("../../Controllers/chat/allMessage")
-// const sendMessage = require("../../Controllers/chat/sendMessage")
+const sendMessage = require("../../Controllers/chat/sendMessage")
 router.route("/:chatId").get(allMessage);
 router.route("/").post(sendMessage);
 
--- backend/Schema/message.js
+++ backend/Schema/message.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const messageSchema = new Schema(
-  {
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-    content: { type: String, trim: true },
-    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
-  },
-  { timestamps: true }
+    {
+        sender: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+        content: { type: String, trim: true },
+        chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+        readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+    },
+    { timestamps: true }
 );
 
 const Message = mongoose.model("Message", messageSchema);
