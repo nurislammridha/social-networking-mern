753026430d206bac460bae608899a4dca1f7b3c3
nurislammridha <mridha851@gmail.com>
2024-01-18 14:38:18 +0600

    up

--- backend/Routes/api/message.js
+++ backend/Routes/api/message.js
@@ -3,10 +3,10 @@ const express = require("express");
 
 const router = express.Router();
 const auth = require("../../middleware/auth")
-// const allMessage = require ("../../Controllers/chat/allMessage")
-// // const sendMessage = require("../../Controllers/chat/sendMessage")
-// router.route("/:chatId").get(allMessage);
-// router.route("/").post(sendMessage);
+const allMessage = require("../../Controllers/chat/allMessage")
+// const sendMessage = require("../../Controllers/chat/sendMessage")
+router.route("/:chatId").get(allMessage);
+router.route("/").post(sendMessage);
 
 
 module.exports = router
